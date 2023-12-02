84c7eb39cd9b80827a7db87f21ee679959ec06d3
phamtanphat <phatdroid94@gmail.com>
2023-12-02 20:44:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/drawableresource11092023/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.drawableresource11092023
+
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
